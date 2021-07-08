974ddee7f908453029ac7f8a219860311a3393c3
MrZhiSponge18 <75477886+MrZhiSponge18@users.noreply.github.com>
2021-07-08 08:51:46 +0200

    Combining PlayerControlelrScript and PlayerMovementScript

--- SSFlex/Assets/Scripts/Player/PlayerController.cs
+++ SSFlex/Assets/Scripts/Player/PlayerController.cs
@@ -6,38 +6,73 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // This script is responsible for:
+    // - Change of Speed
+    // - Walking
+    // - Running
+    // - Sneaking
     // - Jumping
     // - Gravity
-    // - (Potentially sneaking and sliding)
+    // - Movement Animations
+    // - Movement Audio
 
-
-
+    #region Visible
     [Header("References")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
 
-    [Header("Stats")]
+    [Header("Testing Stats")]
+    [SerializeField] private Vector3 moveDir;
+
+    [SerializeField] private float currentPlayerSpeed;
+    [SerializeField] private float playerWalkingSpeed;
+    [SerializeField] private float playerRunSpeed;
+    [SerializeField] private float playerSneakingSpeed;
+
     [SerializeField] private float jumpForce;
+    #endregion
 
-    // Invisible references
+    #region Non-Visible
+    // Non-visible References
+
+    private PlayerShooting shooting;
     private Rigidbody rb;
+    private Animator animator;
 
-    // Invisible stats
+    // Non-visible Stats
+
+    private float movementMultiplier = 1;
+    public float MovementMultiplier { get { return movementMultiplier; } set { movementMultiplier = Mathf.Clamp01(value); } }
+
+    private bool isMoving;
+    private bool isRunning;
+    private bool isSneaking;
+
     private float groundDistance = 0.4f;
-
     private bool isGrounded;
     private bool useGravity = true;
+    #endregion
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        shooting = GetComponent<PlayerShooting>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Responsible for:
-    // - Groundcheck
-    // - Keybinding: Jump
+    private void Start()
+    {
+        currentPlayerSpeed = playerWalkingSpeed;
+    }
+
     private void Update()
     {
+        // Movement
+        MoveDirection();
+
+        PlayerRun();
+        PlayerSneak();
+
+        // Jump
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
@@ -46,20 +81,84 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Responsible for realistic gravity physics.
     void FixedUpdate()
     {
+        rb.velocity = new Vector3(moveDir.x * currentPlayerSpeed * Time.fixedDeltaTime, rb.velocity.y, moveDir.z * currentPlayerSpeed * Time.fixedDeltaTime);
+
         rb.useGravity = false;
         if (useGravity)
         {
             rb.AddForce(Physics.gravity * (rb.mass * rb.mass));
         }
+    }
 
-        
+    #region OnGround_MovementPatterns
+    private void MoveDirection()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        moveDir = transform.forward * ver + transform.right * hor;
+        moveDir.Normalize();
+
+
+        if (moveDir.magnitude >= 0.1f)
+        {
+            isMoving = true;
+
+            if (isRunning)
+            {
+                animator.SetFloat("Velocity", 1f);
+            }
+            else
+            {
+                animator.SetFloat("Velocity", 0.5f);
+            }
+            
+        }
+        else
+        {
+            isMoving = false;
+            animator.SetFloat("Velocity", 0);
+        }
+    }
+
+    private void PlayerRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && isMoving == true && !shooting.ImAiming)
+        {
+            isRunning = true;
+            shooting.InterruptReload();
+            currentPlayerSpeed = playerRunSpeed;
+        }
+        else
+        {
+            currentPlayerSpeed = playerWalkingSpeed * movementMultiplier;
+            isRunning = false;
+        }
+    }
+
+
+    private void PlayerSneak()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && isMoving == true)
+        {
+            currentPlayerSpeed = playerSneakingSpeed;
+            isSneaking = true;
+
+            // No footsteps
+        }
+        else
+        {
+            isSneaking = false;
+        }
     }
 
     private void Jump()
     {
         rb.AddForce(new Vector3(0, jumpForce));
     }
+    #endregion
+
+
 }
