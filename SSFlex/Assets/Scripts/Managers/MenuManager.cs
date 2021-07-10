ddb4de839819d4c206c57b588be1c81cd5e5f90c
MrZhiSponge18 <75477886+MrZhiSponge18@users.noreply.github.com>
2021-07-10 23:05:07 +0200

    NetworkProgramming + UI
    + Photon2Free AssetPack
    + Main Menu Scene and UI
       + Main Menu menu
       + Loading menu
       + Creating Room menu
       + Finding Room menu
       + Error menu
    + Launcher Script
    + MenuManager Script
    + Menu script
    + Only enable MasterClient to start game
    + Visualizing room
    + Everything works
    
    Missing comments on some scripts

---
+++ SSFlex/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    [SerializeField] Menu[] menus;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    // Takes a string and open its menu.
+    public void OpenMenu(string _menuName)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == _menuName)
+            {
+                OpenMenu(menus[i]);
+            }
+            else if (menus[i].isOpen)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    // Takes a menu script and opens its menu.
+    public void OpenMenu(Menu _menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].isOpen)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+
+        _menu.Open();
+    }
+
+    public void CloseMenu(Menu _menu)
+    {
+        _menu.Close();
+    }
+}
---
+++ SSFlex/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public string menuName;
+    public bool isOpen;
+
+    // Enables UI
+    public void Open()
+    {
+        isOpen = true;
+        gameObject.SetActive(true);
+    }
+
+    // Disables UI
+    public void Close()
+    {
+        isOpen = false;
+        gameObject.SetActive(false);
+    }
+}
