a84d362969ae46bc8f3f0c4e7f5e700efab619d8
Flexflext <felix.hereth.fh@googlemail.com>
2021-07-15 20:25:25 +0200

    Shooting better reload + reload stations

---
+++ SSFlex/Assets/Scripts/Shooting/WeaponRefill.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponRefill : MonoBehaviour
+{
+    [SerializeField] private float timeTillNewRefill;
+    [SerializeField] private GameObject gunDisplay;
+    private Collider triggerCollider;
+
+    private void Start()
+    {
+        triggerCollider = GetComponent<Collider>();
+        gunDisplay.SetActive(true);
+    }
+
+    private IEnumerator C_RefillTimer()
+    {
+        yield return new WaitForSeconds(timeTillNewRefill);
+        triggerCollider.enabled = true;
+        gunDisplay.SetActive(true);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 17)
+        {
+            triggerCollider.enabled = false;
+            StartCoroutine(C_RefillTimer());
+            gunDisplay.SetActive(false);
+        }
+        
+    }
+}
