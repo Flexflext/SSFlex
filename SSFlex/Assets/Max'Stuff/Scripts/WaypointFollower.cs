e8e6c76ac81fedf69cc4af8c590b9b1e05e49243
MaxKaul <75678247+MaxKaul@users.noreply.github.com>
2021-07-17 14:10:33 +0200

    Main Menu
    Changes on the WPF

--- SSFlex/Assets/Max'Stuff/Scripts/WaypointFollower.cs
+++ SSFlex/Assets/Max'Stuff/Scripts/WaypointFollower.cs
@@ -8,27 +8,51 @@ public class WaypointFollower : MonoBehaviour
     private List<GameObject> mWaypoints;
 
     [SerializeField]
-    private float mMovementSpeed;
+    private float mMaxMovementSpeed;
+    private float mCurrentMovementSpeed;
     [SerializeField]
-    private float mDamping;
+    private float mMovementccelaration;
+
+
+    [SerializeField]
+    private float mMaxDamping;
+    private float mCurrentDamping;
+    [SerializeField]
+    private float mDampingModifier;
+
+    [SerializeField]
+    private float mStartDelay;
 
     private int mWaypointIdx;
 
     private void Update()
     {
+        if (mStartDelay <= 0)
+        {
+            if (mCurrentMovementSpeed < mMaxMovementSpeed)
+                mCurrentMovementSpeed += mMovementccelaration * Time.deltaTime;
+
+            if(mCurrentDamping < mMaxDamping)
+                mCurrentDamping += mDampingModifier * Time.deltaTime;
+
+            FollowWaypoints();
+        }
+        else
+            mStartDelay -= Time.deltaTime;   
+    }
+      
+    private void FollowWaypoints()
+    {
         if (transform.position != mWaypoints[mWaypointIdx].transform.position)
         {
-            transform.position = Vector3.MoveTowards(transform.position, mWaypoints[mWaypointIdx].transform.position, mMovementSpeed * Time.deltaTime);
-            
+            transform.position = Vector3.MoveTowards(transform.position, mWaypoints[mWaypointIdx].transform.position, mCurrentMovementSpeed * Time.deltaTime);
 
             Quaternion rotation = Quaternion.LookRotation(mWaypoints[mWaypointIdx].transform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * mDamping);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * mCurrentDamping);
         }
         else mWaypointIdx++;
 
         if (mWaypointIdx >= mWaypoints.Count)
             mWaypointIdx = 0;
-
-        Debug.Log(mWaypoints[mWaypointIdx]);
     }
 }
