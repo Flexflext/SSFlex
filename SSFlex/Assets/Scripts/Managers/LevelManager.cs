9f56870ccd5d2ec455e223a15d106f3594d895ea
Flexflext <felix.hereth.fh@googlemail.com>
2021-07-21 23:47:07 +0200

    Anfang Lv Manager

---
+++ SSFlex/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LevelManager : MonoBehaviourPunCallbacks
+{
+    public static LevelManager Instance;
+
+    private Dictionary<int, bool> playerDead = new Dictionary<int, bool>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    public void UpdateDictionary(int _id, bool _lifebool)
+    {
+        photonView.RPC("RPC_UpdateDictionary", RpcTarget.All, _id, _lifebool);
+    }
+
+    [PunRPC]
+    private void RPC_UpdateDictionary(int _id, bool _lifebool)
+    {
+        playerDead.Add(_id, _lifebool);
+    }
+
+    public bool CheckIfTheOnlyOneAlive()
+    {
+        int peopleAlive = 0;
+
+        foreach (var player in playerDead)
+        {
+            if (player.Value)
+            {
+                peopleAlive++;
+            }
+        }
+
+        if (peopleAlive < 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int CheckIfWon()
+    {
+        foreach (var player in playerDead)
+        {
+            if (player.Value)
+            {
+                return player.Key;
+            }
+        }
+
+        return 0;
+    }
+
+}
