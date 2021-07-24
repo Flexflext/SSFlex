f8338f9383f3d34806f24e08261aec8beae16049
MrZhiSponge18 <75477886+MrZhiSponge18@users.noreply.github.com>
2021-07-24 14:41:11 +0200

    Some fixes

---
+++ SSFlex/Assets/Scripts/Audio/StepDestroyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            StartCoroutine(AutoDestroyer());
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            StartCoroutine(AutoDestroyer());
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            StartCoroutine(AutoDestroyer());
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            StartCoroutine(AutoDestroyer());
+        }
+    }
+    private IEnumerator AutoDestroyer()
+    {
+        yield return new WaitForSeconds(0.1f);
+        Destroy(this.gameObject);
+    }
+}
